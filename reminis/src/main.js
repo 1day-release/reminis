2086252b5a5ba103c418a5f100f542772397a22f
nakayama-kirin <kirin.n.0115@gmail.com>
2019-12-29 17:58:51 +0900

    font-awesome, ressを用意

--- reminis/src/main.js
+++ reminis/src/main.js
@@ -3,10 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import 'ress'
 
 Vue.config.productionTip = false
 
 require('@/assets/styles/styles.scss')
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas, fab)
+Vue.component('fa-icon', FontAwesomeIcon)
 
 new Vue({
   router,
