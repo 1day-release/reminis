62d2a3d09d6713365c109f49f262e2498d1a5ba7
nakayama-kirin <kirin.n.0115@gmail.com>
2020-01-08 20:14:20 +0900

    Modal Headerを作成

--- reminis/src/components/essentials/ModalHeader.vue
+++ reminis/src/components/essentials/ModalHeader.vue
@@ -1,17 +1,12 @@
 <template>
   <header>
     <div
-      class="l-header"
-      :class="[setPageClass()]"
+      class="l-modal-header"
     >
       <h1 class="header-title">{{ pageTitle }}</h1>
       <IconButton
-        class="header-button is-left"
-        icon="plus"
-      />
-      <IconButton
-        class="header-button"
-        icon="sort-amount-up-alt"
+        class="header-button is-close"
+        icon="times"
       />
     </div>
   </header>
@@ -21,7 +16,7 @@
   import IconButton from '@/components/common/IconButton.vue'
 
   export default {
-    name: 'Header',
+    name: 'ModalHeader',
     components: {
       IconButton,
     },
@@ -30,13 +25,6 @@
       pageClass: String,
     },
     methods: {
-      setPageClass () {
-        if(this.pageClass) {
-          return ['is-' + this.pageClass]
-        } else {
-          return []
-        }
-      },
     },
   }
 </script>
@@ -44,38 +32,19 @@
 <style scoped lang="scss">
   @import "@/assets/styles/functions.scss";
 
-  .l-header {
+  .l-modal-header {
     position: fixed;
     z-index: 70;
     top: 0;
     left: 0;
     box-sizing: content-box;
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
     width: 100%;
     height: $header-height;
+    background-color: #fff;
     box-shadow: 0 3px 6px $shadow;
-
-    &.is-future {
-      color: #fff;
-      background-color: map-get($color-brand, "future");
-    }
-
-    &.is-reminis {
-      color: #fff;
-      background-color: map-get($color-brand, "reminis");
-    }
-
-    &.is-setting {
-      justify-content: center;
-
-      .header {
-        &-button {
-          display: none;
-        }
-      }
-    }
   }
 
   .header {
@@ -86,14 +55,19 @@
     &-button {
       $size: 30px;
 
+      position: absolute;
       flex: 0 0 auto;
       width: $size;
       height: $size;
       margin: 0 15px;
       background-color: transparent;
 
-      &.is-left {
-        order: -1;
+      &.is-close {
+        position: absolute;
+        top: 0;
+        left: 10px;
+        bottom: 0;
+        margin: auto 0;
       }
     }
   }
